9d8f59b6d6fdaa96199142d2ad05fe3223cb94f3
Pablo Gámez <pablo.gamez@optivamedia.com>
2022-10-14 13:22:32 +0200

    Initial commit

---
+++ app/src/main/java/es/pagamez/pokedex/MainActivity.kt
@@ -0,0 +1,18 @@
+package es.pagamez.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.pagamez.pokedex.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/es/pagamez/pokedex/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package es.pagamez.pokedex.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
